deadaf9cfd47877bd62919dcf66cac76c1a1c4fb
Letera Tujo <letera9tujo@gmail.com>
2026-03-06 13:50:57 +0300

    complet

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting backend and frontend...
-
-start "Backend API" cmd /k "cd backend && python app.py"
-start "Frontend" cmd /k "cd frontend && npm run dev"
-
-echo Both servers are starting in separate windows...
